515ca0588455780cc34cefcaed52414e1cdde979
karateproject <tnkaratedream2024@gmail.com>
2025-04-01 23:12:37 +0530

    change normal

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    outDir: 'dist', // This is the default, you can change it if you need.
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+})
