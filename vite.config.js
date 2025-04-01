e3075c30aab7aff508b92a08be0242fe885b3a8b
karateproject <tnkaratedream2024@gmail.com>
2025-04-01 22:59:06 +0530

    ./

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    outDir: 'dist', // This is the default, you can change it if you need.
+  },
+};
